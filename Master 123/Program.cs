4dd4e7d22a4fd7dac51f3e4a4ceb4b77994b5a21
reevom <reevoomm3@gmail.com>
2023-11-21 13:18:08 +0100

    sadsadsad

--- Master 123/Program.cs
+++ Master 123/Program.cs
@@ -7,6 +7,9 @@ namespace Master_123
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console     dsada
+
         }
     }
 }
